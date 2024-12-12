fc561bade74145689d7b2eaf499e1138bc17f611
tom <tniu@infomedia.com>
2024-12-12 22:15:15 +1100

    Change backend

--- app/backend_hello/main.py
+++ app/backend_hello/main.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
+    # 获取服务器的主机名
+    server_name = socket.gethostname()
     # 获取服务器的 IP 地址
-    server_ip = socket.gethostbyname(socket.gethostname())
-    return jsonify({"message": "Hello, World!", "from": server_ip})
+    server_ip = socket.gethostbyname(server_name)
+    return jsonify({
+        "message": "Hello, World!",
+        "from": {
+            "server_name": server_name,
+            "server_ip": server_ip
+        }
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
