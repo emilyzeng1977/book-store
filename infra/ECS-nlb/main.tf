a5fdec311b148f14ad138e6010f6a3c4f46b0d50
emily <tom.niu8@gmail.com>
2025-06-13 15:14:36 +1000

    BS-6-SG-configured three SGs

--- infra/ECS-nlb/main.tf
+++ infra/ECS-nlb/main.tf
@@ -12,7 +12,7 @@ module "apigw" {
 
   project_name       = var.project_name
   private_subnet_ids = [module.network.private_subnet_id]
-  ecs_service_sg_id  = aws_security_group.ecs_service_sg.id
+  ecs_service_sg_id  = aws_security_group.vpc_link_sg.id
   nlb_listener_arn   = aws_lb_listener.nlb_listener.arn
 }
 
@@ -21,6 +21,6 @@ module "ecs" {
 
   project_name                 = var.project_name
   bookStore_service_subnet_ids = [module.network.private_subnet_id]
-  bookStore_service_sg_ids     = [aws_security_group.ecs_service_sg.id]
+  bookStore_service_sg_ids     = [aws_security_group.ecs_fargate_sg.id]
   bookStore_lb_tg_arn          = aws_lb_target_group.ecs_tg.arn
 }
