485eded2d34f68babcacef4e52ea0f77fb78a648
tom <tniu@infomedia.com>
2025-06-16 11:54:48 +1000

    route

--- infra/ECS-nlb/apigateway-module/route.tf
+++ infra/ECS-nlb/apigateway-module/route.tf
@@ -9,14 +9,3 @@ resource "aws_apigatewayv2_route" "auth_proxy_route" {
   route_key = "ANY /auth/{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.vpc_link_integration.id}"
 }
-# resource "aws_apigatewayv2_route" "books_proxy_route" {
-#   api_id    = aws_apigatewayv2_api.http_api.id
-#   route_key = "ANY /books/{proxy+}"
-#   target    = "integrations/${aws_apigatewayv2_integration.vpc_link_integration.id}"
-# }
-#
-# resource "aws_apigatewayv2_route" "auth_proxy_route" {
-#   api_id    = aws_apigatewayv2_api.http_api.id
-#   route_key = "ANY /auth/{proxy+}"
-#   target    = "integrations/${aws_apigatewayv2_integration.vpc_link_integration.id}"
-# }
