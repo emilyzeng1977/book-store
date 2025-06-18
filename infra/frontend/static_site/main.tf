8047e239795e968da177d2ba92a5556d8de758ed
tom <tniu@infomedia.com>
2025-06-18 11:49:48 +1000

    static website

--- infra/frontend/static_site/main.tf
+++ infra/frontend/static_site/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # 创建 S3 桶，用于托管静态网站
-resource "aws_s3_bucket" "static_site" {
+resource "aws_s3_bucket" "frontend" {
   bucket = var.bucket_name
   force_destroy = true
 
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "static_site" {
 
 # 取消 S3 桶的“公共访问阻止”设置，允许公开访问
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.static_site.id
+  bucket = aws_s3_bucket.frontend.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -25,14 +25,14 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 # 设置桶策略，允许所有用户读取桶中的对象（公开访问）
 resource "aws_s3_bucket_policy" "public_read" {
   depends_on = [aws_s3_bucket_public_access_block.public_access]
-  bucket = aws_s3_bucket.static_site.id
+  bucket = aws_s3_bucket.frontend.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
       Effect    = "Allow",
       Principal = "*",
       Action    = ["s3:GetObject"],
-      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
     }]
   })
 }
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_policy" "public_read" {
 resource "aws_s3_object" "website_files" {
   for_each = fileset("${var.frontend_src}", "**")
 
-  bucket = aws_s3_bucket.static_site.bucket
+  bucket = aws_s3_bucket.frontend.bucket
   key    = each.value
 
   source = "${var.frontend_src}/${each.value}"
@@ -58,7 +58,7 @@ resource "aws_s3_object" "website_files" {
 
 # 配置 S3 桶为静态网站托管，设置默认首页和错误页
 resource "aws_s3_bucket_website_configuration" "static_site" {
-  bucket = aws_s3_bucket.static_site.id
+  bucket = aws_s3_bucket.frontend.id
 
   index_document {
     suffix = "index.html"
--- infra/frontend/static_site/outputs.tf
+++ infra/frontend/static_site/outputs.tf
@@ -1,3 +1,7 @@
 output "website_url" {
-  value = "http://${aws_s3_bucket.static_site.bucket}.s3-website-${var.aws_region}.amazonaws.com"
+  value = "http://${aws_s3_bucket.frontend.bucket}.s3-website-${var.aws_region}.amazonaws.com"
+}
+
+output "domain_name" {
+  value = "${aws_s3_bucket.frontend.bucket}.s3-website-${var.aws_region}.amazonaws.com"
 }
