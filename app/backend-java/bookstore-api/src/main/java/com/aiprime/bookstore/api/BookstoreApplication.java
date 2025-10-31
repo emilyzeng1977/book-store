83992896e15a414d440d9fbbee58811679608a4e
tom <tniu@infomedia.com>
2025-10-31 22:08:16 +1100

    book-store

--- app/backend-java/bookstore-api/src/main/java/com/aiprime/bookstore/api/BookstoreApplication.java
+++ app/backend-java/bookstore-api/src/main/java/com/aiprime/bookstore/api/BookstoreApplication.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class BookstoreApplication {
+	@GetMapping("/")
+	public String index() {
+		return "Welcome to Bookstore API!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
